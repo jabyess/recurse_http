6c672a311b8203f56f371f56cfa9825972dce1c0
James Byess <james.byess@gmail.com>
2019-03-31 15:49:54 -0400

    it works mostly

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from http.server import HTTPServer, socketserver
 HOSTNAME, PORT = "localhost", 4000
 
 
-class Handler(socketserver.BaseRequestHandler):
+class Handler(socketserver.StreamRequestHandler):
 
     store = {}
-    
+    response = b'ok'
+
     def parse_kv(self, input):
         val = input.split('?')[1].split('=')
         if val[0] == 'key' and self.request_action == "GET":
@@ -29,37 +30,32 @@ class Handler(socketserver.BaseRequestHandler):
         return False
 
     def handle(self):
-        self.data = str(self.request.recv(1024).strip()).split(" ")
+        req = self.rfile.readline().decode('ascii').strip()
+        self.data = req.split(" ")
         self.request_action = self.determine_action(self.data[1])
 
         if self.request_action == "GET":
             key = self.parse_kv(self.data[1])
-            self.response = self.store[key]
+            if key in self.store:
+                self.response = self.store[key].encode('ascii')
+            else:
+                self.response = f'no key {key} in store'.encode('ascii')
 
         elif self.request_action == "SET":
             vals = self.parse_kv(self.data[1])
             self.store[vals[0]] = vals[1]
-            # print(vals)
-            self.response = 200
+            self.response = '200'.encode('ascii')
             print(self.store)
 
-        print(self.data)
-        self.request.send(b'ok')
-        self.request.close()
+        self.wfile.write(self.response)
         self.finish()
-        # self.request.close
-
-    # def finish(self):
-    #     pass
-
-
 
 
 if __name__ == '__main__':
 
     recurse_server = HTTPServer((HOSTNAME, PORT), Handler)
 
-    print(f'starting server on port {PORT}')
-    # print(recurse_server.get_request())
+    print(f'starting server on {HOSTNAME}:{PORT}')
+
     recurse_server.serve_forever()
 
